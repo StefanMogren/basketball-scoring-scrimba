e1a165ed1d28e404fec34e1e90836503b5231dff
Stefan Mogren <stefan_mogren@hotmail.com>
2024-11-28 19:20:07 +0100

    Created constants for button and score in js

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
-document.getElementById("count").innerText = 5;
+// The buttons
+const btn1Home = document.getElementById('btn-1-home-html');
+const btn2Home = document.getElementById('btn-2-home-html');
+const btn3Home = document.getElementById('btn-3-home-html');
 
-let count = 0;
+const btn1Away = document.getElementById('btn-1-away-html');
+const btn2Away = document.getElementById('btn-2-away-html');
+const btn3Away = document.getElementById('btn-3-away-html');
 
-console.log(count);
+const btnNewGame = document.getElementById('btn-new-game-html');
 
-// console.log(myAge)
-let myAge = 34;
+const scoreHome = document.getElementById('score-home-html')
+const scoreGuest = document.getElementById('score-guest-html')
 
-console.log(myAge)
+
+
+function addScore() {
+
+}
