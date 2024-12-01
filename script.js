b705a44a5278faefc6a27886708301c0901526a9
Stefan Mogren <stefan_mogren@hotmail.com>
2024-12-01 18:53:02 +0100

    Point buttons now add to score, new game resets score

--- script.js
+++ script.js
@@ -1,19 +1,50 @@
 // The buttons
-const btn1Home = document.getElementById('btn-1-home-html');
-const btn2Home = document.getElementById('btn-2-home-html');
-const btn3Home = document.getElementById('btn-3-home-html');
+// const btn1Home = document.getElementById('btn-1-home-html');
+// const btn2Home = document.getElementById('btn-2-home-html');
+// const btn3Home = document.getElementById('btn-3-home-html');
 
-const btn1Away = document.getElementById('btn-1-away-html');
-const btn2Away = document.getElementById('btn-2-away-html');
-const btn3Away = document.getElementById('btn-3-away-html');
+const buttonHome = document.querySelectorAll('.home-button');
+buttonHome.forEach((btn)=>{
+    btn.addEventListener('click', addHomeScore);
+});
 
-const btnNewGame = document.getElementById('btn-new-game-html');
+const buttonGuest = document.querySelectorAll('.guest-button');
+buttonGuest.forEach((btn)=>{
+    btn.addEventListener('click', addGuestScore);
+});
 
 const scoreHome = document.getElementById('score-home-html')
 const scoreGuest = document.getElementById('score-guest-html')
 
+const btnNewGame = document.getElementById('btn-new-game-html');
+btnNewGame.addEventListener('click', restartScore);
 
 
-function addScore() {
 
+
+function addHomeScore() {
+    if(this.innerText === '+1' || this.innerText === '+2' || this.innerText === '+3' ) {
+        let btnToNum = parseInt(this.innerText.slice(1));
+        let scoreToNum = parseInt(scoreHome.innerText);
+        scoreHome.innerText = scoreToNum + btnToNum;
+    }
+    else {
+        scoreHome.innerText = 'che at!'
+    }
+}
+
+function addGuestScore() {
+    if(this.innerText === '+1' || this.innerText === '+2' || this.innerText === '+3' ) {
+        let btnToNum = parseInt(this.innerText.slice(1));
+        let scoreToNum = parseInt(scoreGuest.innerText);
+        scoreGuest.innerText = scoreToNum + btnToNum;
+    }
+    else {
+        scoreGuest.innerText = 'che at!'
+    }
+}
+
+function restartScore() {
+    scoreHome.innerText = "0";
+    scoreGuest.innerText = "0";
 }
