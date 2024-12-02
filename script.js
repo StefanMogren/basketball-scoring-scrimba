95d6d999b454aec83ead01c307a760cecac92b90
Stefan Mogren <stefan_mogren@hotmail.com>
2024-12-02 08:22:22 +0100

    Added comments to explain what happens in JavaScript

--- script.js
+++ script.js
@@ -1,27 +1,38 @@
 // The buttons
-// const btn1Home = document.getElementById('btn-1-home-html');
-// const btn2Home = document.getElementById('btn-2-home-html');
-// const btn3Home = document.getElementById('btn-3-home-html');
-
+// Every button that adds score to the Home team will trigger the function addHomeScore.
+// addHomeScore will check the content of the button itself and only use the value (namely 1, 2, and 3) in order to add the score.
 const buttonHome = document.querySelectorAll('.home-button');
 buttonHome.forEach((btn)=>{
     btn.addEventListener('click', addHomeScore);
 });
 
+
+// Every button that adds score to the Guest team will trigger the function addGuestScore.
+// addGuestScore will check the content of the button itself and only use the value (namely 1, 2, and 3) in order to add the score.
 const buttonGuest = document.querySelectorAll('.guest-button');
 buttonGuest.forEach((btn)=>{
     btn.addEventListener('click', addGuestScore);
 });
 
+
+// The score for both the Home and Guest team.
 const scoreHome = document.getElementById('score-home-html')
 const scoreGuest = document.getElementById('score-guest-html')
 
+
+// Button to start a new game.
+// Runs the function restartScore which sets the score for both teams to 0.
 const btnNewGame = document.getElementById('btn-new-game-html');
 btnNewGame.addEventListener('click', restartScore);
 
 
 
-
+// After you press the Home score buttons, the function will first check to see if the content of said buttons are either +1, +2, or +3.
+// An anti-cheat function prevents people from modifying the buttons on the web browser itself.
+// The function then removes the + and turns the string into a number.
+// The function also turns the content of scoreHome into a number.
+// The function finally adds the values together and returns them into scoreHome.
+// Finally, if this team has a score higher than the opponent, the border around the score turns yellow.
 function addHomeScore() {
     if(this.innerText === '+1' || this.innerText === '+2' || this.innerText === '+3' ) {
         let btnToNum = parseInt(this.innerText.slice(1));
@@ -38,6 +49,13 @@ function addHomeScore() {
     }
 }
 
+
+// After you press the Guest score buttons, the function will first check to see if the content of said buttons are either +1, +2, or +3.
+// An anti-cheat function prevents people from modifying the buttons on the web browser itself.
+// The function then removes the + and turns the string into a number.
+// The function also turns the content of scoreGuest into a number.
+// The function finally adds the values together and returns them into scoreGuest.
+// Finally, if this team has a score higher than the opponent, the border around the score turns yellow.
 function addGuestScore() {
     if(this.innerText === '+1' || this.innerText === '+2' || this.innerText === '+3' ) {
         let btnToNum = parseInt(this.innerText.slice(1));
@@ -53,6 +71,9 @@ function addGuestScore() {
     }
 }
 
+
+// Resets the score of both teams to 0.
+// Also removes the yellow border if it's present.
 function restartScore() {
     scoreHome.innerText = "0";
     scoreGuest.innerText = "0";
